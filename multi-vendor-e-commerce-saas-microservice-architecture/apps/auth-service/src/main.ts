5c1cf5b623e322b4e1980d90d0095d8f21532472
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-08-21 12:00:55 +0600

    feat: add error handling middleware and AppError class for improved error management.

--- multi-vendor-e-commerce-saas-microservice-architecture/apps/auth-service/src/main.ts
+++ multi-vendor-e-commerce-saas-microservice-architecture/apps/auth-service/src/main.ts
@@ -1,14 +1,31 @@
+import { errorMiddleware } from "./../../../packages/error-handler/error-middleware";
 import express from "express";
-
-const host = process.env.HOST ?? "localhost";
-const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send({ message: "Hello API." });
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+app.use(errorMiddleware);
+
+const host = process.env.HOST ?? "localhost";
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+
+const server = app.listen(port, host, () => {
+  console.log(`Auth service listening at http://${host}:${port}`);
 });
+
+server.on("Server Error", console.error);
