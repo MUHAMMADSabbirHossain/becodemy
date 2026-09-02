6c4c0ee049191c1828291bdf59af896f58d68e0b
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-09-02 22:24:46 +0600

    feat: refactor auth-service main file to improve middleware setup and logging.

--- eshop-multivendor-ecommerce-microservice/apps/auth-service/src/main.ts
+++ eshop-multivendor-ecommerce-microservice/apps/auth-service/src/main.ts
@@ -4,12 +4,25 @@
  */
 
 import { errorHandler } from '@eshop-multivendor-ecommerce-microservice/error-handler';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
 import * as path from 'path';
 
 const app = express();
+const port = process.env.PORT || 6001;
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API!' });
@@ -19,8 +32,7 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to auth-service!' + ' ' + errorHandler() });
 });
 
-const port = process.env.PORT || 6001;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Auth service is listening at http://localhost:${port}/api`);
 });
-server.on('error', console.error);
+server.on('error', (err) => console.error('Server error', err));
