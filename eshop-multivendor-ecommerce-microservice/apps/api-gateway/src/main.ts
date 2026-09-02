1373f00fceda44fe6a72f0c19c5e461e53d721bc
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-09-02 22:16:56 +0600

    feat: enhance api-gateway and auth-service with middleware, health check, and improved request handling cors, morgan, rate-limit and proxy etc.

--- eshop-multivendor-ecommerce-microservice/apps/api-gateway/src/main.ts
+++ eshop-multivendor-ecommerce-microservice/apps/api-gateway/src/main.ts
@@ -1,14 +1,65 @@
-import express from 'express';
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import express from 'express';
+import * as path from 'path';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
+import morgan from 'morgan';
+// import swaggerUi from 'swagger-ui-express';
+// import axios from 'axios';
+import cookieParser from 'cookie-parser';
+import { ipKeyGenerator, rateLimit } from 'express-rate-limit';
 
 const app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // Origin of the frontend application
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true, // Allow cookies
+  }),
+);
+app.use(morgan('dev'));
+app.use(
+  express.json({
+    limit: '100mb', // Limit request body json size
+  }),
+);
+app.use(
+  express.urlencoded({
+    limit: '100mb', // Limit request field size
+    extended: true,
+  }),
+);
+app.use(cookieParser());
+app.set('trust proxy', 1); // trust first proxy for IP address
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req: any) => (req.user ? 1000 : 100), // limit each IP to 100 requests per windowMs
+  message: 'Too many requests, please try again later!',
+  standardHeaders: true,
+  legacyHeaders: true,
+  // keyGenerator: (req: any) => req.ip, // ValidationError: Custom keyGenerator appears to use request IP without calling the ipKeyGenerator helper function for IPv6 addresses. This could allow IPv6 users to bypass limits. See https://express-rate-limit.github.io/ERR_ERL_KEY_GEN_IPV6/ for more information.
+  keyGenerator: (req: any) => {
+    return ipKeyGenerator(req.ip);
+  },
+});
+app.use(limiter);
+
+app.get('/gateway-health', (req, res) => {
+  res.send({ message: 'Welcome to api-gateway!' });
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+app.use('/', proxy('http://localhost:6001'));
+
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
 });
+server.on('error', console.error);
--- eshop-multivendor-ecommerce-microservice/apps/auth-service/src/main.ts
+++ eshop-multivendor-ecommerce-microservice/apps/auth-service/src/main.ts
@@ -11,11 +11,15 @@ const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to auth-service!' + '' + errorHandler() });
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello API!' });
 });
 
-const port = process.env.PORT || 3333;
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to auth-service!' + ' ' + errorHandler() });
+});
+
+const port = process.env.PORT || 6001;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
