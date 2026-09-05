3e7736ec9305c052234f4bcb7648e64ef5cbfe60
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-09-06 00:08:36 +0600

    chore(user-ui): update package.json for development scripts and add new dependencies
    - Modified the "dev" script to include both "serve" and "dev" targets.
    - Added new scripts: "plugin:list" and "user-ui".
    - Updated dependencies: added Tailwind CSS, Next.js, React, and PostCSS.
    - Updated devDependencies: added ESLint plugins and testing libraries.
    
    feat: add postcss configuration for Tailwind CSS
    
    - Created postcss.config.mjs to configure Tailwind CSS as a PostCSS plugin.
    
    fix: update tsconfig.json to include user-ui and user-ui-e2e paths
    
    - Added paths for user-ui and user-ui-e2e to the tsconfig.json for better module resolution.

---
+++ eshop-multivendor-ecommerce-microservice/apps/user-ui/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <>
+      <h1 className="uppercase text-3xl text-red-500 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
