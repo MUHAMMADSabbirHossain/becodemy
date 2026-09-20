e491bc21345bd0d1084967f61f85b2297f7ecf7c
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2026-09-20 16:16:34 +0600

    feat(components): add new components library with initial setup
    - Downgraded express to version 4.21.2 for compatibility.
    - Added Babel configuration for React components.
    - Created a new components library with basic structure and example components.
    - Implemented a sample component and server component for demonstration.
    - Updated TypeScript configuration for the new components package.
    - Included README for unit testing instructions.

---
+++ eshop-multivendor-ecommerce-microservice/packages/components/README.md
@@ -0,0 +1,7 @@
+# @eshop-multivendor-ecommerce-microservice/components
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @eshop-multivendor-ecommerce-microservice/components` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ eshop-multivendor-ecommerce-microservice/packages/components/src/lib/components.tsx
@@ -0,0 +1,11 @@
+import styles from './components.module.css';
+
+export function EshopMultivendorEcommerceMicroserviceComponents() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to EshopMultivendorEcommerceMicroserviceComponents!</h1>
+    </div>
+  );
+}
+
+export default EshopMultivendorEcommerceMicroserviceComponents;
